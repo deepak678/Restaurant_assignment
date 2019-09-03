8e6525f389425bfe8777c463a09448a78d312f0e
Deepak Manchanda (MINDTREE LIMITED) <v-deeman@microsoft.com>
2019-09-03 11:19:52 +0530

    final commit

--- script.bat
+++ script.bat
@@ -3,5 +3,5 @@ pip install passlib
 pip install smtplib
 START cmd /c python add_user_and_login.py
 START cmd /c python Booking.py
-START cmd /c python Billing
+START cmd /c python Billing.py
 PAUSE
